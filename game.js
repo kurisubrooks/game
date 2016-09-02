adad11e1ac79a75c7ac28a74dd71b28226cbb873
Kurisu Brooks <kurisubrooks@gmail.com>
2016-09-02 19:53:03 +1000

    Plugins, TILED Map Loader, P2 Physics, Collision, Updated Objects Spawner

--- game.js
+++ game.js
@@ -5,11 +5,24 @@ let map
 window.onload = function() {
     let game = new Phaser.Game("100%", "100%", Phaser.CANVAS, "game", {
         preload: function() {
-            let preload = this
+            // Plugins
+            this.game.add.plugin(Phaser.Plugin.Debug)
+            this.game.add.plugin(Phaser.Plugin.Tiled)
 
+            let preload = this
+            let cacheKey = Phaser.Plugin.Tiled.utils.cacheKey
+
+            // Map
+            this.game.load.tiledmap(cacheKey("overworld", "tiledmap"), "assets/maps/city.json", null, Phaser.Tilemap.TILED_JSON)
+            this.game.load.image(cacheKey("overworld", "tileset", "rpg"), "assets/sheets/rpg.png")
+
+            /*
             // Map
             this.load.tilemap("world", "assets/maps/city.json", null, Phaser.Tilemap.TILED_JSON)
             this.load.image("spritesheet", "assets/sheets/rpg.png")
+            */
+
+            // Assets
             this.load.image("player", "assets/player.png")
 
             // Objects
@@ -43,37 +56,26 @@ window.onload = function() {
 
         create: function() {
             // Physics
-            this.game.physics.startSystem(Phaser.Physics.ARCADE)
+            this.game.physics.startSystem(Phaser.Physics.P2JS)
+            this.game.physics.p2.restitution = 0
 
             // Map
-            this.map = this.game.add.tilemap("world")
-            this.map.addTilesetImage("rpg", "spritesheet")
-            map = this.map
+            map = this.map = this.game.add.tiledmap("overworld")
 
-            // Tiles
-            layers.base = this.map.createLayer("base")
-            layers.buildings = this.map.createLayer("buildings")
-            layers.roof = this.map.createLayer("roof")
-
-            // Objects
-            layers.shadows = this.map.createLayer("shadows")
-            layers.objects = this.map.createLayer("objects")
-            layers.collision = this.map.createLayer("collision")
-
-            //this.map.setCollisionBetween(1, 2000, true, "collision")
-            layers.base.resizeWorld()
-
-            objects = this.game.add.group()
-            objects.enableBody = true
-
-            this.spawnObjects("objects")
-            this.spawnObjects("collision")
+            // Collision
+            this.game.physics.p2.convertTiledCollisionObjects(map, "collision")
 
             // Player
             let result = this.findObjects(this.map, "objects", "spawnPoint")
             this.player = this.game.add.sprite(result[0].x, result[0].y, "player")
-            this.game.physics.arcade.enable(this.player)
+            this.game.physics.p2.enable(this.player)
             this.game.camera.follow(this.player)
+
+            objects = this.game.add.group()
+            objects.enableBody = true
+
+            // Objects
+            this.spawnObjects("objects")
 
             // Movement
             this.cursors = this.game.input.keyboard.createCursorKeys()
@@ -84,6 +86,7 @@ window.onload = function() {
             let speed = 250
 
             this.player.body.collideWorldBounds = true
+            this.player.body.fixedRotation = true
             this.player.body.velocity.x = 0
             this.player.body.velocity.y = 0
 
@@ -105,15 +108,12 @@ window.onload = function() {
             } else if (this.game.input.keyboard.isDown(Phaser.Keyboard.D) || this.cursors.right.isDown) {
                 this.player.body.velocity.x += speed
             }
-
-            // Collision
-            this.game.physics.arcade.collide(this.player, this.blockedLayer)
-            this.game.physics.arcade.collide(this.player, this.underBlockedLayer)
         },
 
         render: function() {
             // Player Bounds
-            //this.game.debug.body(this.player)
+            this.game.debug.body(this.player)
+            this.game.debug.body(objects)
 
             // FPS
             this.game.debug.text(game.time.fps, 2, 15, "#FFFF00")
@@ -131,7 +131,7 @@ window.onload = function() {
         // Spawn Items
         spawnObjects: function(layer) {
             let item
-            let result = this.findObjects(this.map, layer)
+            let result = this.findObjects(map, layer)
 
             result.forEach(function(element) {
                 this.createObject(element, objects)
@@ -142,21 +142,24 @@ window.onload = function() {
         findObjects: function(map, layer, type) {
             let result = []
 
-            map.objects[layer].forEach(function(element) {
-                if (element.properties) {
-                    if (type) {
-                        if (element.properties.type === type) {
+            map.objects.forEach(function(title) {
+                if (title.name === layer) {
+                    title.objects.forEach(function(element) {
+                        if (type) {
+                            if (element.properties) {
+                                if (element.properties.type === type) {
+                                    element.y -= map.tileHeight
+                                    result.push(element)
+                                }
+                            }
+                        } else {
                             element.y -= map.tileHeight
                             result.push(element)
                         }
-                    } else {
-                        element.y -= map.tileHeight
-                        result.push(element)
-                    }
+                    })
                 }
             })
 
-            console.log(result)
             return result
         },
 
